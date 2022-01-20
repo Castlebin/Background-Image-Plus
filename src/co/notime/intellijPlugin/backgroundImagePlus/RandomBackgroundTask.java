2ccd87fcc122c220dec9f174f841ca6400fd13d4
heller <castlebin@gmail.com>
2022-01-20 10:58:42 +0800

    update

--- src/co/notime/intellijPlugin/backgroundImagePlus/RandomBackgroundTask.java
+++ src/co/notime/intellijPlugin/backgroundImagePlus/RandomBackgroundTask.java
@@ -2,41 +2,36 @@ package co.notime.intellijPlugin.backgroundImagePlus;
 
 import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.wm.impl.IdeBackgroundUtil;
-
 import java.io.File;
 
 public class RandomBackgroundTask implements Runnable {
-    private final ImagesHandler imagesHandler = new ImagesHandler();
+    private ImagesHandler imagesHandler = new ImagesHandler();
 
     public void run() {
         BingImage.download();
 
         PropertiesComponent prop = PropertiesComponent.getInstance();
         String folder = prop.getValue("BackgroundImagesFolder");
-        if (folder == null || folder.isEmpty()) {
-            folder = BingImage.DEFAULT_BG_DIR;
-        }
+        if (folder != null && !folder.isEmpty()) {
+            File file = new File(folder);
+            if (!file.exists()) {
+                NotificationCenter.notice("Image folder not set");
+            } else {
+                String image = this.imagesHandler.getRandomImage(folder);
+                if (image == null) {
+                    NotificationCenter.notice("No image found");
+                } else {
+                    if (image.contains(",")) {
+                        NotificationCenter.notice("Intellij wont load images with ',' character\n" + image);
+                    }
 
-        File file = new File(folder);
-        if (!file.exists()) {
+                    prop.setValue("idea.background.frame", (String)null);
+                    prop.setValue("idea.background.editor", image);
+                    IdeBackgroundUtil.repaintAllWindows();
+                }
+            }
+        } else {
             NotificationCenter.notice("Image folder not set");
-            return;
         }
-
-        String image = imagesHandler.getRandomImage(folder);
-        if (image == null) {
-            NotificationCenter.notice("No image found");
-            return;
-        }
-
-        if (image.contains(",")) {
-            NotificationCenter.notice("Intellij wont load images with ',' character\n" + image);
-            return;
-        }
-
-        prop.setValue("idea.background.frame", null);
-        prop.setValue("idea.background.editor", image);
-        IdeBackgroundUtil.repaintAllWindows();
     }
-
 }
